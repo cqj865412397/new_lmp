00c1318482e13c86c89bd3c54db19d8829d395c9
君哥哥 <@windows10.microdone.cn>
2018-11-13 21:34:07 +0800

    删除鸟飞出来的内容

--- lmq-mui/src/main/java/com/lmq/dao/ExamplepriceMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lmq.dao;
-
-import com.lmq.domain.Exampleprice;
-
-public interface ExamplepriceMapper {
-    int insert(Exampleprice record);
-
-    int insertSelective(Exampleprice record);
-}
--- lmq-mui/src/main/java/com/lmq/mui/controller/ModuleController.java
+++
@@ -1,5 +0,0 @@
-package com.lmq.mui.controller;
-
-public class ModuleController {
-
-}
