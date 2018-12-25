946a9ca50650a71e7fe796520120524cca1a46a4
zjf0109 <1838228605@qq.com>
2018-12-25 12:38:00 +0800

    添加客户等级

---
+++ lmq-mui/src/main/java/com/lmq/service/CustomerlevelService.java
@@ -0,0 +1,9 @@
+package com.lmq.service;
+
+import java.util.List;
+
+import com.lmq.domain.Customerlevel;
+
+public interface CustomerlevelService {
+	 List<Customerlevel> queryCustomerlevelBySid(Integer sid);
+}
